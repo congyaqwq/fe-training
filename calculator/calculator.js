40bec70c411d389e1fd284e3648f19147a001894
zhi <15070657624@163.com>
2024-11-30 11:40:12 +0800

    feat:clear

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,39 +1,77 @@
 let buttons = document.querySelectorAll('.button');
 
 let display = document.querySelector('.display');
-let record='';
-let i=0;
+let record = [];
+let i = 0;
 
 function calc() {
-  const command = display.innerText.replace('x', '*')
+	const command = display.innerText.replace('x', '*')
 
-  const res = eval(command);
-  display.innerText = res;
+	const res = eval(command);
+	display.innerText = res;
 }
-buttons.forEach((button) =>(
+
+function displayFn() {
+	display.innerText = record.join('')
+}
+
+
+
+buttons.forEach((button) => (
 	button.addEventListener('click', (e) => {
+		const text = e.target.innerText
 		/*let numberinput=document.getElementById('numberinput').value;
 		record+=record;不知道怎么把每次按钮按的数存到record中
 		i++;*/
-		if (e.target.innerText === '=') {
+		console.log(e)
+		console.log(e.target)
+		if (text === '=') {
 			calc()
+			calcInput()
 			return;
 		}
-		if (display.innerText === 'result') {
-			display.innerText = e.target.innerText;
+		// if (display.innerText === 'result') {
+		// 	display.innerText = text;
+		// 	record.push(text)
+		// 	return;
+		// }
+		if (text === 'clear') {
+			record = []
+			displayFn()
+
+			onChange('')
 			return;
 		}
-		if(e.target.innerText==='clear'){
-			display.innerHTML=null;
-			return;
+		if (text === 'back') {
+			record.pop()
+			console.log(record)
+			displayFn()
+
+
+			onChange(input.value.slice(input.value.length - 1))
+			return
 		}
-		// if(e.target.innerText==='back'){
-		// 	display.innerHTML=null;
-		// 	record[i]=null;
-		// 	return record;
-				
-		// 	}
-		const addText = e.target.innerText;
-		display.innerText = display.innerText + addText;
+
+		onChange(input.value + text)
+		record.push(text)
+		console.log(record)
+		displayFn()
 	})
 ));
+
+const input = document.querySelector('input')
+let value = '';
+function onChange(v) {
+	value = v
+	input.value = v
+	console.log(input.value)
+}
+
+function calcInput() {
+	const res = eval(input.value.replace('x', '*'))
+	onChange(res)
+}
+
+input.addEventListener('input', (e) => {
+	onChange(e.target.value)
+})
